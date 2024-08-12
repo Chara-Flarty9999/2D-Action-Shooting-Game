263725a4366228f6437808898d83bf9e28411670
Yukinori-Kuwahara <chara.flarty999@gmail.com>
2024-08-13 02:13:20 +0900

    [update]プレイヤーが乗った時に作動するリフトを作成、コンポーネントでリフトの挙動作成可能に。

--- Assets/Script/LiftManager.cs
+++ Assets/Script/LiftManager.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class LiftManager : MonoBehaviour
 {
+    /// <summary>初期動作を設定できる。</summary>
+    [SerializeField] liftmode liftmoving = default;
+    int _liftmode;
+    [SerializeField] bool playOnCollision = default;
+    Animator m_anim = default;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_anim = GetComponent<Animator>();
+        _liftmode = (int)liftmoving;
+        Debug.Log(_liftmode);
+        m_anim.SetInteger("Liftmoving", _liftmode);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_anim.SetBool("PlayOnCollision", playOnCollision);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (playOnCollision == true)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                playOnCollision = false;
+            }
+        }
+    }
+
+    enum liftmode
+    {
+        /// <summary>
+        /// 上に動く。
+        /// </summary>
+        Up, 
+        /// <summary>
+        /// 下に動く。
+        /// </summary>
+        Down, 
+        /// <summary>
+        /// 左に動く。
+        /// </summary>
+        Left, 
+        /// <summary>
+        /// 右に動く。
+        /// </summary>
+        Right,
     }
 }
